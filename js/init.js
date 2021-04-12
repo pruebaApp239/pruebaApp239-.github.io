79e5e4135bd288db2faf280f3d1cdb0fb6d909f9
pruebaApp239 <74285779+pruebaApp239@users.noreply.github.com>
2021-04-12 00:19:53 -0500

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,14 +1,9 @@
 // @ts-ignore
 firebase.initializeApp({
-var firebaseConfig = {
-    var firebaseConfig = {
     apiKey: "AIzaSyDuwdLXim0FyoRMdfXEdWfmJbtu81rNpaw",
     authDomain: "pwa-iot-6a844.firebaseapp.com",
     projectId: "pwa-iot-6a844",
     storageBucket: "pwa-iot-6a844.appspot.com",
     messagingSenderId: "969288659485",
     appId: "1:969288659485:web:4f2176706f8de33b9e8cf4"
-  };
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
 });
